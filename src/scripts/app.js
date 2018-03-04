cd0b779ef3bb4e4fb4fc3e2af7371276350cd166
arturmarszycki <artur.marszycki@gmail.com>
2018-03-04 22:24:54 +0100

    add slider and some animations in jQuery

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,56 @@
 $(function () {
 
     $('.nav-menu').children().mouseover(function() {
-        $(this).find('.drop-down-menu').show();
+        $(this).find('.drop-down-menu').css('display', 'block');
     });
     $('.nav-menu').children().mouseout(function() {
-        $(this).find('.drop-down-menu').hide();
+        $(this).find('.drop-down-menu').css('display', 'none');
     });
 
+    $('.move-left').click(function() {
+        $('.slider-black').toggle('slide');
+        $('.slider-red').toggle('slide');
+        $('.slider-red').css('display', 'flex');
+    });
+    $('.move-right').click(function() {
+        $('.slider-black').toggle('slide');
+        $('.slider-red').toggle('slide');
+        $('.slider-red').css('display', 'flex');
+    });
+
+    $('.picture').eq(0).mouseover(function() {
+        $(this).find('.picture-header').slideUp();
+    });
+    $('.picture').eq(1).mouseover(function() {
+        $(this).find('.picture-header').slideUp();
+    });
+    $('.picture').eq(0).mouseout(function() {
+        $(this).find('.picture-header').slideDown();
+    });
+    $('.picture').eq(1).mouseout(function() {
+        $(this).find('.picture-header').slideDown();
+    });
+
+    $('.offert').mouseover(function() {
+        $(this).css({'background-color': '#4fd4a9', 'border': '2px solid #4fd4a9'});
+        $(this).find('.offert-kind').css('color', '#fff');
+        $(this).addClass('change');
+        $(this).find('.offert-price').addClass('change-small');
+        $(this).find('.offert-price').css({'color': '#4fd4a9', 'border': '2px solid #4fd4a9'});
+        $(this).find('button').css({'color': '#4fd4a9', 'background-color': '#fff'});
+        $(this).find('ul li').eq(0).css('background-color', '#fff');
+        $(this).find('ul li').eq(2).css('background-color', '#fff');
+    });
+    $('.offert').mouseout(function() {
+        $(this).css({'background-color': '#f4f4f4', 'border': '2px solid #aaa'});
+        $(this).find('.offert-kind').css('color', '#4fd4a9');
+        $(this).removeClass('change');
+        $(this).find('.offert-price').removeClass('change-small');
+        $(this).find('.offert-price').css({'color': '#888', 'border': '2px solid #999'});
+        $(this).find('button').css({'color': '#fff', 'background-color': '#4fd4a9'});
+        $(this).find('ul li').eq(0).css('background-color', '#ddd');
+        $(this).find('ul li').eq(2).css('background-color', '#ddd');
+    });
 
     $('.agree-img').click(function(){
         $(this).find('img').toggle();
